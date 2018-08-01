c9f128d38cc6883ce7b8a2eae95f8f369806c854
yurikbv <yurikbv@yandex.ru>
2018-08-01 18:33:47 +0300

    add new project j-wood

---
+++ smartgrid.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let smartgrid = require('smart-grid');
+
+/* It's principal settings in smart grid project */
+let settings = {
+  outputStyle: 'scss',
+  columns: 24, /* number of grid columns */
+  offset: '30px', /* gutter width px || % || rem */
+  mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
+  tab: '  ',
+  container: {
+    maxWidth: '1600px', /* max-width оn very large screen */
+    fields: '30px' /* side fields */
+  },
+  breakPoints: {
+    md: {
+      width: '960px'
+    },
+    sm: {
+      width: '780px',
+      fields: '15px' /* set fields only if you want to change container.fields */
+    },
+    xs: {
+      width: '560px'
+    }
+    /*
+    We can create any quantity of break points.
+
+    some_name: {
+        width: 'Npx',
+        fields: 'N(px|%|rem)',
+        offset: 'N(px|%|rem)'
+    }
+    */
+  }
+};
+
+smartgrid('./app/scss', settings);
